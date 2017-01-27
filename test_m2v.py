0910388f17ebe4c9a10e0f8f684b2ddad8575ce8
Takashi Ueno <ueno.takashi.75w@st.kyoto-u.ac.jp>
2017-01-27 10:37:29 +0900

    Use Average Precision

--- test_m2v.py
+++ test_m2v.py
@@ -11,15 +11,26 @@ genres = ['blues', 'classical', 'country', 'disco', 'hiphop',
 dirname = os.path.join("mus2vecs", "mus2vec_%d_%d" % (nceps, dim))
 
 sup = 20
-hit = 0
+# hit = 0
 tot = 0
+ave_precision = 0
 for g in genres:
     for i in range(100):
         fin = os.path.join(dirname, g, g + ".%05d.npy" % i)
         mus2vec_list = mus2vec.similar_list(fin, dirname)
+        hit = 0
+        ap = 0
         for j in range(sup):
             if g in mus2vec_list[j]:
                 hit += 1
-        tot += 20
+                ap += hit / float(i + 1)
+        if hit == 0:
+            ap = 0
+        else:
+            ap /= float(hit)
+        # tot += 20
+        tot += 1
+        ave_precision += ap
 
-print "%f%% (%d/%d)" % (hit/float(tot)*100, hit, tot)
+# print "%f%% (%d/%d)" % (hit/float(tot)*100, hit, tot)
+print "AP@%d : %f (total : %d)" % (sup, ave_precision / float(tot), tot)
--- test_mfcc.py
+++ test_mfcc.py
@@ -9,15 +9,26 @@ genres = ['blues', 'classical', 'country', 'disco', 'hiphop',
           'jazz', 'metal', 'pop', 'reggae', 'rock']
 
 sup = 20
-hit = 0
+# hit = 0
 tot = 0
+ave_precision = 0
 for g in genres:
     for i in range(100):
         fin = os.path.join('gtzan', g, g + ".%05d_%d.ceps.npy" % (i, nceps))
         mfcc_list = mfcc_cos.similar_list(fin, nceps)
+        hit = 0
+        ap = 0
         for j in range(sup):
             if g in mfcc_list[j]:
                 hit += 1
-        tot += 20
+                ap += hit / float(i + 1)
+        if hit == 0:
+            ap = 0
+        else:
+            ap /= float(hit)
+        # tot += 20
+        tot += 1
+        ave_precision += ap
 
-print "%f%% (%d/%d)" % (hit/float(tot)*100, hit, tot)
+# print "%f%% (%d/%d)" % (hit/float(tot)*100, hit, tot)
+print "AP@%d : %f (total : %d)" % (sup, ave_precision / float(tot), tot)
