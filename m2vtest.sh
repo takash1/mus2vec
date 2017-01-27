e8e18acdfd35ae314fdf6bdd0b64c949554f32dd
Takashi Ueno <ueno.takashi.75w@st.kyoto-u.ac.jp>
2017-01-27 13:04:20 +0900

    改行の追加

--- m2vtest.sh
+++ m2vtest.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 echo 'mfcc13 ae100'
 python test_m2v.py 13 100
-echo 'mfcc13 ae300'
+echo '\nmfcc13 ae300'
 python test_m2v.py 13 300
-echo 'mfcc13 ae500'
+echo '\nmfcc13 ae500'
 python test_m2v.py 13 500
-echo 'mfcc20 ae100'
+echo '\nmfcc20 ae100'
 python test_m2v.py 20 100
-echo 'mfcc20 ae300'
+echo '\nmfcc20 ae300'
 python test_m2v.py 20 300
-echo 'mfcc20 ae500'
+echo '\nmfcc20 ae500'
 python test_m2v.py 20 500
-echo 'mfcc39 ae100'
+echo '\nmfcc39 ae100'
 python test_m2v.py 39 100
-echo 'mfcc39 ae300'
+echo '\nmfcc39 ae300'
 python test_m2v.py 39 300
-echo 'mfcc39 ae500'
+echo '\nmfcc39 ae500'
 python test_m2v.py 39 500
