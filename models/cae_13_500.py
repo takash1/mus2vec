635a6c1ad6bf71ed68163dc908bb17fa5a6fa5a7
Takashi Ueno <ueno.takashi.75w@st.kyoto-u.ac.jp>
2017-01-27 16:28:30 +0900

    Add Convlution Layer

--- models/cae_13_500.py
+++ models/cae_13_500.py
@@ -14,12 +14,14 @@ class ConvAE(Chain):
     def __init__(self):
         super(ConvAE, self).__init__(
             enc1=L.Convolution2D(1, 100, (1, NCEPS)),
-            enc2=L.Convolution2D(100, 10, (1, 1)),
-            enc3=L.Convolution2D(10, 1000, (3000, 1)),
-            enc4=L.Convolution2D(1000, DIM, (1, 1)),
-            dec4=L.Deconvolution2D(DIM, 1000, (1, 1)),
-            dec3=L.Deconvolution2D(1000, 10, (3000, 1)),
-            dec2=L.Deconvolution2D(10, 100, (1, 1)),
+            enc2=L.Convolution2D(100, 50, (1, NCEPS)),
+            enc3=L.Convolution2D(50, 10, (1, 1)),
+            enc4=L.Convolution2D(10, 1000, (3000, 1)),
+            enc5=L.Convolution2D(1000, DIM, (1, 1)),
+            dec5=L.Deconvolution2D(DIM, 1000, (1, 1)),
+            dec4=L.Deconvolution2D(1000, 10, (3000, 1)),
+            dec3=L.Deconvolution2D(10, 50, (1, 1)),
+            dec2=L.Deconvolution2D(50, 100, (1, 1)),
             dec1=L.Deconvolution2D(100, 1, (1, NCEPS))
         )
 
@@ -34,10 +36,12 @@ class ConvAE(Chain):
         e2 = F.relu(self.enc2(e1))
         e3 = F.relu(self.enc3(e2))
         e4 = F.relu(self.enc4(e3))
-        return e4
+        e5 = F.relu(self.enc5(e4))
+        return e5
 
     def decode(self, x):
-        d4 = F.relu(self.dec4(x))
+        d5 = F.relu(self.dec4(x))
+        d4 = F.relu(self.dec4(d5))
         d3 = F.relu(self.dec3(d4))
         d2 = F.relu(self.dec2(d3))
         d1 = self.dec1(d2)
