360d1c7a3da4cea03a02c2ff5d93e5b50b2cb019
Takashi Ueno <ueno.takashi.75w@st.kyoto-u.ac.jp>
2017-01-27 16:18:21 +0900

    k-meansのkを追加

--- emdtest.sh
+++ emdtest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo 'mfcc13'
-python test_emd.py 13
+python test_emd.py 13 16
 echo '\nmfcc20'
-python test_emd.py 20
+python test_emd.py 20 30
 echo '\nmfcc30'
-python test_emd.py 30
+python test_emd.py 30 50
