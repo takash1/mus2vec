173742402b8711229a5f4c1508ad801bb33dcae7
Takashi Ueno <ueno.takashi.75w@st.kyoto-u.ac.jp>
2017-01-27 18:30:45 +0900

    Change to Stacked AE

--- models/cae_20_500.py
+++ models/cae_20_500.py
@@ -25,24 +25,38 @@ class ConvAE(Chain):
             dec1=L.Deconvolution2D(100, 1, (1, NCEPS))
         )
 
-    def __call__(self, x):
-        return F.mean_squared_error(self.fwd(x), x)
+    def __call__(self, x, layer):
+        return F.mean_squared_error(self.fwd(x, layer), x)
 
-    def fwd(self, x):
-        return self.decode(self.encode(x))
+    def fwd(self, x, layer=5):
+        return self.decode(self.encode(x, layer), layer)
 
-    def encode(self, x):
-        e1 = F.relu(self.enc1(x))
-        e2 = F.relu(self.enc2(e1))
-        e3 = F.relu(self.enc3(e2))
-        e4 = F.relu(self.enc4(e3))
-        e5 = F.relu(self.enc5(e4))
-        return e5
+    def encode(self, x, layer):
+        e = F.relu(self.enc1(x))
+        if layer == 1:
+            return e
+        e = F.relu(self.enc2(e))
+        if layer == 2:
+            return e
+        e = F.relu(self.enc3(e))
+        if layer == 3:
+            return e
+        e = F.relu(self.enc4(e))
+        if layer == 4:
+            return e
+        e = F.relu(self.enc5(e))
+        if layer == 5:
+            return e
 
-    def decode(self, x):
-        d5 = F.relu(self.dec5(x))
-        d4 = F.relu(self.dec4(d5))
-        d3 = F.relu(self.dec3(d4))
-        d2 = F.relu(self.dec2(d3))
-        d1 = self.dec1(d2)
-        return d1
+    def decode(self, x, layer):
+        if layer == 5:
+            x = F.relu(self.dec5(x))
+        if layer >= 4:
+            x = F.relu(self.dec4(x))
+        if layer >= 3:
+            x = F.relu(self.dec3(x))
+        if layer >= 2:
+            x = F.relu(self.dec2(x))
+        if layer >= 1:
+            d = self.dec1(x)
+        return d
